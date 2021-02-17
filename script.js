03b9caea807eaa4a756aac63f0da105be5e3bbd0
margareta75 <margareta.xx@gmail.com>
2021-02-17 23:36:04 +0100

    Changes in project (animation js, design, little code fixing)

---
+++ script.js
@@ -0,0 +1,11 @@
+var i = 0;
+var txt = ' design and code, and I love doing it.';
+var speed = 50;
+
+function typeWriter() {
+  if (i < txt.length) {
+    document.getElementById("write-text").innerHTML += txt.charAt(i);
+    i++;
+    setTimeout(typeWriter, speed);
+  }
+}
